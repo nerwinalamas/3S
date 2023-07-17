06c676c6059c1a1b4eb2220694fc5a2597a246cd
unknown <nerwinalamas@gmail.com>
2023-07-17 10:14:16 +0800

    minor changes in Nav bar and Form validation

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,10 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import { useContext } from "react";
 import { ProductContext } from "../context/ProductContext";
 import { Link } from "react-router-dom";
 
+const contactDetails = {
+  name: "",
+  contact: "",
+  address: "",
+};
+
 const Cart = () => {
   const { cart, handleRemoveAll } = useContext(ProductContext);
+  const [inputForm, setInputForm] = useState(contactDetails);
 
   const totalCost = () => {
     let total = 0;
@@ -12,6 +19,14 @@ const Cart = () => {
       total += item.price;
     });
     return total;
+  };
+
+  const handleForm = () => {
+    if (!(inputForm.name && inputForm.contact && inputForm.address)) {
+      alert("Error: Please fill the required fields.");
+    } else {
+      handleRemoveAll();
+    }
   };
 
   return (
@@ -33,7 +48,7 @@ const Cart = () => {
                 <div className="w-full flex flex-col justify-center">
                   <p className="text-sm">{items.title} </p>
                   <p className="text-lg text-red-700 font-medium">
-                    ${items.price}{" "}
+                    ${items.price}
                   </p>
                   {/* <div className="flex items-center justify-start pl-10 gap-5">
                     <p>-</p>
@@ -49,22 +64,37 @@ const Cart = () => {
 
       <div>
         {cart.length > 0 && (
-          <div className="h-52 w-80 flex flex-col justify-center items-center gap-2 my-5 md:mt-10">
+          <div className="h-auto w-80 flex flex-col justify-center items-center gap-2 my-5 md:mt-10">
             <input
               type="text"
               placeholder="Name"
+              name="name"
+              value={inputForm.name}
+              onChange={(e) =>
+                setInputForm({ ...inputForm, name: e.target.value })
+              }
               required
               className="w-64 h-8 p-2 rounded bg-slate-100"
             />
             <input
               type="text"
               placeholder="Contact no."
+              name="contact"
+              value={inputForm.contact}
+              onChange={(e) =>
+                setInputForm({ ...inputForm, contact: e.target.value })
+              }
               required
               className="w-64 h-8 p-2 rounded bg-slate-100"
             />
             <input
               type="text"
               placeholder="Address"
+              name="address"
+              value={inputForm.address}
+              onChange={(e) =>
+                setInputForm({ ...inputForm, address: e.target.value })
+              }
               required
               className="w-64 h-8 p-2 rounded bg-slate-100"
             />
@@ -72,13 +102,20 @@ const Cart = () => {
               <p>Total:</p>
               <p className="text-red-700 text-xl">${totalCost().toFixed(2)}</p>
             </div>
-            <Link to={`/completed`}>
-              <button
-                onClick={handleRemoveAll}
-                className="w-auto h-10 px-4 py-2 rounded bg-orange-700 text-white hover:bg-white hover:text-orange-700 hover:border-solid hover:border-2 hover:cursor-pointer"
-              >
-                Confirm
-              </button>
+            <Link
+              to={`${
+                !(inputForm.name && inputForm.contact && inputForm.address)
+                  ? "#"
+                  : "/completed"
+              }`}
+              onClick={handleForm}
+              className={`${
+                !(inputForm.name && inputForm.contact && inputForm.address)
+                  ? "hover:cursor-not-allowed"
+                  : "hover:cursor-pointer"
+              } w-auto h-10 px-4 py-2 rounded bg-orange-700 text-white`}
+            >
+              Confirm
             </Link>
           </div>
         )}
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -8,7 +8,7 @@ const Nav = () => {
   const { cart } = useContext(ProductContext);
 
   return (
-    <div className="flex w-screen justify-between items-center h-14 bg-white px-10 sticky top-0">
+    <div className="flex w-screen justify-between items-center h-14 px-10 z-10 sticky top-0 bg-white">
       <Link to={`/`} title="Tripple S" className="font-bold text-2xl md:text-3xl">3S</Link>
       <Link to={`/cart`} className="flex">
         <AiOutlineShoppingCart className="text-xl mx-1 md:text-2xl"/>
